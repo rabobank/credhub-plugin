7a68acae0e4ee579137dd6e47d472c7f8450adf4
João Viegas <joao.nascimento@rabobank.nl>
2023-08-11 17:53:45 +0200

    generate binaries with the version number

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,7 +3,7 @@
 OUTPUT_DIR=$PWD/dist
 mkdir -p ${OUTPUT_DIR}
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${OUTPUT_DIR}/credhub-plugin-linux-amd64 .
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ${OUTPUT_DIR}/credhub-plugin-darwin-amd64 .
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ${OUTPUT_DIR}/credhub-plugin-darwin-arm64 .
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ${OUTPUT_DIR}/credhub-plugin-window-amd64 .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${OUTPUT_DIR}/credhub-plugin-${VERSION}-linux-amd64 .
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ${OUTPUT_DIR}/credhub-plugin-${VERSION}-darwin-amd64 .
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ${OUTPUT_DIR}/credhub-plugin-${VERSION}-darwin-arm64 .
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ${OUTPUT_DIR}/credhub-plugin-${VERSION}-window-amd64 .
