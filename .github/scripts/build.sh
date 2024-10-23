27c66798c5dc741287489c7966d1fb6d2d139b9a
Harry Metske <harry.metske@gmail.com>
2024-10-23 13:40:53 +0200

    fix github action

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 OUTPUT_DIR=$PWD/dist
-mkdir -p ${OUTPUT_DIR}
+mkdir -p "${OUTPUT_DIR}"
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${OUTPUT_DIR}/credhub-plugin-${VERSION}-linux-amd64 -ldflags "-X github.com/rabobank/credhub-splugin/conf.VERSION=${VERSION} -X github.com/rabobank/credhub-plugin/conf.COMMIT=${COMMIT}" .
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ${OUTPUT_DIR}/credhub-plugin-${VERSION}-darwin-amd64 -ldflags "-X github.com/rabobank/credhub-plugin/conf.VERSION=${VERSION} -X github.com/rabobank/credhub-plugin/conf.COMMIT=${COMMIT}" .
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ${OUTPUT_DIR}/credhub-plugin-${VERSION}-darwin-arm64 -ldflags "-X github.com/rabobank/credhub-plugin/conf.VERSION=${VERSION} -X github.com/rabobank/credhub-plugin/conf.COMMIT=${COMMIT}" .
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ${OUTPUT_DIR}/credhub-plugin-${VERSION}-window-amd64 -ldflags "-X github.com/rabobank/credhub-plugin/conf.VERSION=${VERSION} -X github.com/rabobank/credhub-plugin/conf.COMMIT=${COMMIT}" .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "${OUTPUT_DIR}"/credhub-plugin-"${VERSION}"-linux-amd64 -ldflags "-X github.com/rabobank/credhub-plugin/conf.VERSION=${VERSION} -X github.com/rabobank/credhub-plugin/conf.COMMIT=${COMMIT}" .
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o "${OUTPUT_DIR}"/credhub-plugin-"${VERSION}"-darwin-amd64 -ldflags "-X github.com/rabobank/credhub-plugin/conf.VERSION=${VERSION} -X github.com/rabobank/credhub-plugin/conf.COMMIT=${COMMIT}" .
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o "${OUTPUT_DIR}"/credhub-plugin-"${VERSION}"-darwin-arm64 -ldflags "-X github.com/rabobank/credhub-plugin/conf.VERSION=${VERSION} -X github.com/rabobank/credhub-plugin/conf.COMMIT=${COMMIT}" .
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o "${OUTPUT_DIR}"/credhub-plugin-"${VERSION}"-window-amd64 -ldflags "-X github.com/rabobank/credhub-plugin/conf.VERSION=${VERSION} -X github.com/rabobank/credhub-plugin/conf.COMMIT=${COMMIT}" .
